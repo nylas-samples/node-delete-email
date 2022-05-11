a8f0ac27c517d01609b681aca985a305b0767b2c
Ram Bansal <ram.bansal@nylas.com>
2022-05-11 17:52:04 -0400

    create example

--- build/index.js
+++ build/index.js
@@ -1,10 +1,24 @@
+// Import your dependencies
 import 'dotenv/config';
 import Nylas from "nylas";
+// Configure your Nylas client
 Nylas.config({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
 });
 const nylas = Nylas.with(process.env.ACCESS_TOKEN);
-const currentAccount = await nylas.account.get();
-// @ts-ignore
-console.log({ currentAccount });
+// Provide the message id you want to trash
+const MESSAGE_ID = process.env.MESSAGE_ID;
+const TRASH_LABEL_NAME = 'trash';
+// append the trash label and save the message
+try {
+    const message = await nylas.messages.find(MESSAGE_ID);
+    const labels = await nylas.labels.list();
+    const trashLabel = labels.find(label => label.name === TRASH_LABEL_NAME);
+    await message.labels.push(trashLabel);
+    await message.save();
+    console.log(`Message "${MESSAGE_ID}" was deleted.`);
+}
+catch (error) {
+    console.log('error', error);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+// Import your dependencies
 import 'dotenv/config';
 import Nylas from "nylas"
 
+// Configure your Nylas client
 Nylas.config({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -8,7 +10,20 @@ Nylas.config({
 
 const nylas = Nylas.with(process.env.ACCESS_TOKEN);
 
-const currentAccount = await nylas.account.get();
+// Provide the message id you want to trash
+const MESSAGE_ID = process.env.MESSAGE_ID;
+const TRASH_LABEL_NAME = 'trash';
 
-// @ts-ignore
-console.log({ currentAccount })
+// append the trash label and save the message
+try {
+    const message = await nylas.messages.find(MESSAGE_ID);
+    const labels = await nylas.labels.list();
+
+    const trashLabel = labels.find(label => label.name === TRASH_LABEL_NAME);
+    await message.labels.push(trashLabel);
+    await message.save();
+
+    console.log(`Message "${MESSAGE_ID}" was deleted.`);
+} catch(error) {
+    console.log('error', error)
+}
